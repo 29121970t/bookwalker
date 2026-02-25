8e21f2e9e5dfd0e1a7dd0a1c153c096b751d37c4
29121970t <29121970tttt@gmail.com>
2026-02-25 12:08:58 +0300

    adding lombok

--- src/main/java/com/kruosant/bookwalker/dtos/BookCreateDto.java
+++ src/main/java/com/kruosant/bookwalker/dtos/BookCreateDto.java
@@ -1,7 +1,14 @@
 package com.kruosant.bookwalker.dtos;
 
+import lombok.*;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookCreateDto {
 
   private String name;
@@ -10,109 +17,6 @@ public class BookCreateDto {
   private Date publishDate;
   private String publisher;
 
-
-  public BookCreateDto(String name, String author, long pageCount,
-                       Date publishDate, String publisher) {
-
-    this.name = name;
-    this.author = author;
-    this.pageCount = pageCount;
-    this.publishDate = publishDate;
-    this.publisher = publisher;
-  }
-
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getAuthor() {
-    return author;
-  }
-
-  public void setAuthor(String author) {
-    this.author = author;
-  }
-
-  public long getPageCount() {
-    return pageCount;
-  }
-
-  public void setPageCount(long pageCount) {
-    this.pageCount = pageCount;
-  }
-
-  public Date getPublishDate() {
-    return publishDate;
-  }
-
-  public void setPublishDate(Date publishDate) {
-    this.publishDate = publishDate;
-  }
-
-  public String getPublisher() {
-    return publisher;
-  }
-
-  public void setPublisher(String publisher) {
-    this.publisher = publisher;
-  }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-    private long id;
-    private String name;
-    private String author;
-    private long pageCount;
-    private Date publishDate;
-    private String publisher;
-
-    Builder() {
-    }
-
-
-
-    public Builder name(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public Builder pageCount(long pageCount) {
-      this.pageCount = pageCount;
-      return this;
-    }
-
-    public Builder id(long id) {
-      this.id = id;
-      return this;
-    }
-
-    public Builder publishDate(Date publishDate) {
-      this.publishDate = publishDate;
-      return this;
-    }
-
-    public Builder publisher(String publisher) {
-      this.publisher = publisher;
-      return this;
-    }
-
-    public Builder author(String author) {
-      this.author = author;
-      return this;
-    }
-
-    public BookFullDto build() {
-      return new com.kruosant.bookwalker.dtos.BookFullDto(id, name, author, pageCount,
-          publishDate, publisher);
-    }
-  }
 }
+
 
--- src/main/java/com/kruosant/bookwalker/dtos/BookFullDto.java
+++ src/main/java/com/kruosant/bookwalker/dtos/BookFullDto.java
@@ -1,8 +1,14 @@
 package com.kruosant.bookwalker.dtos;
 
+import lombok.*;
+
 import java.util.Date;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookFullDto {
   private long id;
   private String name;
@@ -10,113 +16,4 @@ public class BookFullDto {
   private long pageCount;
   private Date publishDate;
   private String publisher;
-
-
-  public BookFullDto(long id, String name, String author,
-                     long pageCount, Date publishDate, String publisher) {
-    this.id = id;
-    this.name = name;
-    this.author = author;
-    this.pageCount = pageCount;
-    this.publishDate = publishDate;
-    this.publisher = publisher;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getAuthor() {
-    return author;
-  }
-
-  public void setAuthor(String author) {
-    this.author = author;
-  }
-
-  public long getPageCount() {
-    return pageCount;
-  }
-
-  public void setPageCount(long pageCount) {
-    this.pageCount = pageCount;
-  }
-
-  public Date getPublishDate() {
-    return publishDate;
-  }
-
-  public void setPublishDate(Date publishDate) {
-    this.publishDate = publishDate;
-  }
-
-  public String getPublisher() {
-    return publisher;
-  }
-
-  public void setPublisher(String publisher) {
-    this.publisher = publisher;
-  }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-    private long id;
-    private String name;
-    private String author;
-    private long pageCount;
-    private Date publishDate;
-    private String publisher;
-
-    Builder() {
-    }
-
-    public Builder id(long id) {
-      this.id = id;
-      return this;
-    }
-
-    public Builder name(String name) {
-      this.name = name;
-      return this;
-    }
-
-    public Builder author(String author) {
-      this.author = author;
-      return this;
-    }
-
-    public Builder pageCount(long pageCount) {
-      this.pageCount = pageCount;
-      return this;
-    }
-
-    public Builder publishDate(Date publishDate) {
-      this.publishDate = publishDate;
-      return this;
-    }
-
-    public Builder publisher(String publisher) {
-      this.publisher = publisher;
-      return this;
-    }
-
-    public BookFullDto build() {
-      return new BookFullDto(id, name, author, pageCount, publishDate, publisher);
-    }
-  }
 }
