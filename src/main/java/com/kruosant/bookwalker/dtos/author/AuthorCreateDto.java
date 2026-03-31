d9876201633ed52ad7ca8ce87b655b6a52f2b6a8
29121970t <29121970tttt@gmail.com>
2026-04-01 02:42:38 +0300

    sonar fixes

--- src/main/java/com/kruosant/bookwalker/dtos/author/AuthorCreateDto.java
+++ src/main/java/com/kruosant/bookwalker/dtos/author/AuthorCreateDto.java
@@ -11,15 +11,15 @@ import lombok.*;
 @Builder
 public class AuthorCreateDto {
   @NotNull(message = "field is necessary")
+  @Schema(description = "Author's middle name", example = " ", requiredMode = Schema.RequiredMode.REQUIRED)
+  private String middleName;
+  @NotNull(message = "field is necessary")
   @Schema(description = "Author's name", example = "Jennifer", requiredMode = Schema.RequiredMode.REQUIRED)
   private String name;
-  @Schema(description = "Author's middle name", example = " ", requiredMode = Schema.RequiredMode.REQUIRED)
-  @NotNull(message = "field is necessary")
-  private String middleName;
   @Schema(description = "Author's surname", example = "Garcia", requiredMode = Schema.RequiredMode.REQUIRED)
   @NotNull(message = "field is necessary")
   private String surname;
-  @Schema(description = "Author's biography", example = "Author bio for Jennifer Garcia", requiredMode = Schema.RequiredMode.REQUIRED)
   @NotNull(message = "field is necessary")
+  @Schema(description = "Author's biography", example = "Author bio for Jennifer Garcia", requiredMode = Schema.RequiredMode.REQUIRED)
   private String bio;
 }
--- src/main/java/com/kruosant/bookwalker/dtos/author/AuthorFullDto.java
+++ src/main/java/com/kruosant/bookwalker/dtos/author/AuthorFullDto.java
@@ -18,15 +18,19 @@ public class AuthorFullDto {
   @NotNull(message = "field is necessary")
   @Schema(description = "Author's name", example = "Jennifer", requiredMode = Schema.RequiredMode.REQUIRED)
   private String name;
+
   @Schema(description = "Author's middle name", example = " ", requiredMode = Schema.RequiredMode.REQUIRED)
   @NotNull(message = "field is necessary")
   private String middleName;
+
   @Schema(description = "Author's surname", example = "Garcia", requiredMode = Schema.RequiredMode.REQUIRED)
   @NotNull(message = "field is necessary")
   private String surname;
+
   @Schema(description = "Author's biography", example = "Author bio for Jennifer Garcia", requiredMode = Schema.RequiredMode.REQUIRED)
   @NotNull(message = "field is necessary")
   private String bio;
+
   @Schema(description = "Author's list of books", requiredMode = Schema.RequiredMode.REQUIRED)
   private List<BookBasicInfoDto> books = new ArrayList<>();
 }
